6961ca3483da6dc943e9d7ef314365f3967cf940
GeonWoo <lgw9898@naver.com>
2024-01-20 15:17:50 +0900

    Initial Commit

---
+++ FunchApp/FunchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FunchAppApp.swift
+//  FunchApp
+//
+//  Created by Geon Woo lee on 1/20/24.
+//
+
+import SwiftUI
+
+@main
+struct FunchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
