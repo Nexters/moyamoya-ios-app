5d1c2c6248fca85208b67dde2063fa6fe4749c65
GeonWoo <lgw9898@naver.com>
2024-01-20 20:02:37 +0900

    create home view

--- FunchApp/Presentation/HomeScene/HomeView.swift
+++ FunchApp/Presentation/HomeScene/HomeView.swift
@@ -5,4 +5,14 @@
 //  Created by Geon Woo lee on 1/20/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+#Preview {
+    HomeView()
+}
