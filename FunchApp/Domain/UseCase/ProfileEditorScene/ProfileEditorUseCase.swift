6b3cf1c4ee85a7552277e5af214665ae41e535b9
GeonWoo <lgw9898@naver.com>
2024-01-22 03:05:09 +0900

    implementation profile editor usecase

--- FunchApp/Domain/UseCase/ProfileEditorScene/ProfileEditorUseCase.swift
+++ FunchApp/Domain/UseCase/ProfileEditorScene/ProfileEditorUseCase.swift
@@ -7,12 +7,22 @@
 
 import Foundation
 
-//final class profileEditorUseCase {
-//    private let repositorty: ProfileRepositoryType
-//    
-//    init() {
-//        self.repositorty = repositor
-//    }
-//    
-//    
-//}
+final class ProfileEditorUseCase {
+    private let profileRepository: ProfileRepository
+    
+    init() {
+        self.profileRepository = ProfileRepository()
+    }
+    
+    /// `본인 프로필` 생성
+    func createProfile(createUserQuery: CreateUserQuery, completion: @escaping (Result<Void, Error>) -> Void) {
+        profileRepository.createProfile(createUserQuery: createUserQuery) { result in
+            switch result {
+            case .success(let success):
+                completion(.success(()))
+            case .failure(let failure):
+                completion(.success(()))
+            }
+        }
+    }
+}
