8d3f842c87489b7a272bc8e9a7757c41c937b25e
seongmin221 <seongmin221@naver.com>
2024-01-21 23:58:51 +0900

    implement FlowLayoutGrid

--- FunchApp/Core/DesignSystem/UICompoents.SwiftUI/FlowLayoutGrid.swift
+++ FunchApp/Core/DesignSystem/UICompoents.SwiftUI/FlowLayoutGrid.swift
@@ -5,4 +5,57 @@
 //  Created by 이성민 on 1/21/24.
 //
 
-import Foundation
+import SwiftUI
+
+struct FlowLayoutGrid {
+    var alignment: HorizontalAlignment = .leading
+    var itemSpacing: CGFloat
+    var lineSpacing: CGFloat
+    
+    init(alignment: HorizontalAlignment,
+         itemSpacing: CGFloat,
+         lineSpacing: CGFloat) {
+        self.alignment = alignment
+        self.itemSpacing = itemSpacing
+        self.lineSpacing = lineSpacing
+    }
+}
+
+extension FlowLayoutGrid: Layout {
+    func sizeThatFits(proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) -> CGSize {
+        let width = proposal.replacingUnspecifiedDimensions().width
+        let sizes = subviews.map { $0.sizeThatFits(.unspecified) }
+        return calculateLayout(of: sizes, containerWidth: width).size
+    }
+    
+    func placeSubviews(in bounds: CGRect, proposal: ProposedViewSize, subviews: Subviews, cache: inout ()) {
+        let sizes = subviews.map { $0.sizeThatFits(.unspecified) }
+        let offsets = calculateLayout(of: sizes, containerWidth: bounds.width).offsets
+        for (offset, subview) in zip(offsets, subviews) {
+            subview.place(at: .init(x: offset.x + bounds.minX, y: offset.y + bounds.minY), proposal: .unspecified)
+        }
+    }
+}
+
+extension FlowLayoutGrid {
+    func calculateLayout(of subviewSizes: [CGSize], containerWidth: CGFloat) -> (offsets: [CGPoint], size: CGSize) {
+        var offsets: [CGPoint] = []
+        var containerSize: CGSize = .zero
+        
+        var curPos: CGPoint = .zero
+        for size in subviewSizes {
+            if curPos.x + size.width > containerWidth {
+                curPos.x = 0
+                curPos.y += size.height + lineSpacing
+            }
+            
+            offsets.append(curPos)
+            curPos.x += size.width + itemSpacing
+            
+            containerSize.width = max(containerSize.width, curPos.x)
+            containerSize.height = max(containerSize.height, curPos.y + size.height)
+        }
+        
+        return (offsets, containerSize)
+    }
+}
