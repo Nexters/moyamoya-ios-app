3db8a033a7ddd0c1e7eb5b36a95ca3f857337ab2
seongmin221 <seongmin221@naver.com>
2024-01-22 02:37:11 +0900

    add profile mbti button pair added

---
+++ FunchApp/Presentation/ProfileEditorScene/ProfileMBTIButtonPair.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileMBTIButtonPair.swift
+//  FunchApp
+//
+//  Created by 이성민 on 1/22/24.
+//
+
+import SwiftUI
+
+struct ProfileMBTIButtonPair: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileMBTIButtonPair()
+}
