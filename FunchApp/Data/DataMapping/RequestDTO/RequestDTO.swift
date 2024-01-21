05805071d91760fbabad8b570dacb68a17efdb18
GeonWoo <lgw9898@naver.com>
2024-01-22 00:48:11 +0900

    darft: added profile api (search & create)

---
+++ FunchApp/Data/DataMapping/RequestDTO/RequestDTO.swift
@@ -0,0 +1,10 @@
+//
+//  RequestDTO.swift
+//  FunchApp
+//
+//  Created by Geon Woo lee on 1/21/24.
+//
+
+import Foundation
+
+struct RequestDTO {}
---
+++ FunchApp/Data/DataMapping/ResponseDTO/ResponseDTO.swift
@@ -0,0 +1,10 @@
+//
+//  ResponseDTO.swift
+//  FunchApp
+//
+//  Created by Geon Woo lee on 1/21/24.
+//
+
+import Foundation
+
+struct ResponseDTO {}
