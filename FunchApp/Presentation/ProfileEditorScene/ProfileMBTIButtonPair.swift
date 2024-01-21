0b013c564675d9f750581d73053696119644864b
seongmin221 <seongmin221@naver.com>
2024-01-22 02:40:36 +0900

    implemented profile mbti button pair

--- FunchApp/Presentation/ProfileEditorScene/ProfileMBTIButtonPair.swift
+++ FunchApp/Presentation/ProfileEditorScene/ProfileMBTIButtonPair.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct ProfileMBTIButtonPair: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    
+    var action: (String) -> Void
+    var dataPair: [String]
+    
+    @State var buttonSelected: Int?
+    
+    init(onTap action: @escaping (String) -> Void,
+         dataPair: [String]) {
+        self.action = action
+        self.dataPair = dataPair
     }
-}
-
-#Preview {
-    ProfileMBTIButtonPair()
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                ForEach(0..<2) { i in
+                    Button {
+                        action(dataPair[i])
+                        self.buttonSelected = i
+                    } label: {
+                        Text(dataPair[i])
+                            .foregroundStyle(buttonSelected == i ? Color.black : Color.gray)
+                            .frame(maxWidth: .infinity, maxHeight: .infinity)
+                            .background(buttonSelected == i ? Color.gray : Color.clear)
+                    }
+                    .buttonStyle(.noEffect)
+                }
+            }
+            
+            DashedLine()
+                .stroke(style: .init(dash: [3]))
+                .fill(Color(red: 0.73, green: 0.73, blue: 0.73))
+                .frame(height: 2)
+        }
+        .frame(width: 48, height: 96)
+        .background(Color(red: 0.96, green: 0.96, blue: 0.96))
+        .clipShape(RoundedRectangle(cornerRadius: 16))
+    }
 }
