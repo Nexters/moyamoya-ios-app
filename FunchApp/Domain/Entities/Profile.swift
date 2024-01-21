be9d3f67c3344a03658b99123893730424d76c3c
GeonWoo <lgw9898@naver.com>
2024-01-21 12:53:05 +0900

    implemented a profile view. (need to change to multiple chipView)

--- FunchApp/Domain/Entities/Profile.swift
+++ FunchApp/Domain/Entities/Profile.swift
@@ -16,9 +16,9 @@ struct Profile {
     var birth: String
     
     /// 직군
-    var major: String
+    var major: [Major]
     /// 동아리
-    var club: String
+    var club: [Club]
     /// mbti
     var mbti: String
     /// 별자리
@@ -26,17 +26,35 @@ struct Profile {
     /// 지하철 역
     var subwayName: String
     
-//    /// 기본값
-//    static var defaultValue: Profile {
-//        return Profile(
-//            userCode: <#T##String#>,
-//            userNickname: <#T##String#>,
-//            birth: <#T##String#>,
-//            major: <#T##String#>,
-//            club: <#T##String#>,
-//            mbti: <#T##String#>,
-//            constellation: <#T##String#>,
-//            subwayName: <#T##String#>
-//        )
-//    }
+    /// 전공
+    struct Major {
+        /// 전공 이름
+        var name: String
+        /// 전공 이미지
+        var imageName: String
+    }
+    
+    /// 동아리
+    struct Club {
+        /// 동아리 이름
+        var name: String
+        /// 동아리 이미지
+        var imageName: String
+    }
+}
+
+extension Profile {
+    /// 작업에 있어 테스트 가능한 값 (테스트 코드에 사용하면 안됩니다.)
+    static var testableValue: Profile {
+        return Profile(
+            userCode: "#2X87T",
+            userNickname: "넥스터즈다모임",
+            birth: "20021225",
+            major: [.init(name: "개발자", imageName: "plus")],
+            club: [.init(name: "넥스터즈", imageName: "plus")],
+            mbti: "ESTP",
+            constellation: "물병",
+            subwayName: "강남"
+        )
+    }
 }
--- FunchApp/Presentation/ProfileScene/ProfileView.swift
+++ FunchApp/Presentation/ProfileScene/ProfileView.swift
@@ -8,67 +8,137 @@
 import SwiftUI
 
 struct ProfileView: View {
-    let items: [String] = ["1", "2", "3"]
+    @Environment(\.dismiss) var dismiss
+    
+    let profile: Profile = .testableValue
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 20) {
-            Text("USERCODE")
-                .font(.system(size: 14))
-                .foregroundColor(Color(red: 0.49, green: 0.49, blue: 0.48))
-            
-            Text("동대문역사문화공원역")
-                .font(.system(size: 20, weight: .bold))
-                .foregroundColor(Color(red: 0.68, green: 0.68, blue: 0.68))
-                .frame(maxWidth: .infinity, alignment: .topLeading)
-            
-            HStack(spacing: 0) {
-                Text("직군")
-                    .multilineTextAlignment(.leading)
-                    .lineLimit(0)
+        VStack {
+            Spacer()
+                .frame(height: 8)
+            VStack(alignment: .leading, spacing: 20) {
+                Text("USERCODE")
                     .font(.system(size: 14))
-                    .foregroundColor(Color(red: 0.18, green: 0.18, blue: 0.18))
-                    .frame(width: 52, alignment: .leading)
+                    .foregroundColor(Color(red: 0.49, green: 0.49, blue: 0.48))
                 
-//                Spacer()
-//                    .frame(width: 14)
+                Text("동대문역사문화공원역")
+                    .font(.system(size: 20, weight: .bold))
+                    .foregroundColor(Color(red: 0.68, green: 0.68, blue: 0.68))
+                    .frame(maxWidth: .infinity, alignment: .topLeading)
                 
-                ChipView(title: "Nexters", imageName: "")
-                
-                Spacer()
+                ProfileChipRow(.직군, .testableValue)
+                ProfileChipRow(.동아리, .testableValue)
+                ProfileChipRow(.MBTI, .testableValue)
+                ProfileChipRow(.별자리, .testableValue)
+                ProfileChipRow(.지하철, .testableValue)
             }
-//            .padding(.horizontal, 20)
+            .padding(.horizontal, 20)
+            .padding(.vertical, 24)
+            .frame(width: 320, alignment: .center)
+            .background(.white)
+            .cornerRadius(20)
+            .overlay(
+                RoundedRectangle(cornerRadius: 20)
+                    .inset(by: 0.5)
+                    .stroke(Color(red: 0.89, green: 0.89, blue: 0.89), lineWidth: 1)
+            )
             
-            Grid(alignment: .leading) {
-                GridRow {
-                    Text("직군")
-                        .frame(width: 52, alignment: .leading)
-//                        .lineLimit(0)
-                        .font(.system(size: 14))
-                        .foregroundColor(Color(red: 0.18, green: 0.18, blue: 0.18))
-                        .background(.red)
-                        
-                    ChipView(title: "Nexters", imageName: "")
-                    
-//                    Spacer()
+            Spacer()
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    // action
+                } label: {
+                    Text("피드백 보내기")
+                        .font(.system(size: 14, weight: .regular))
+                        .foregroundColor(.white)
+                        .padding(.horizontal, 12)
+                        .padding(.vertical, 8)
+                        .background(.black)
+                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
                 }
             }
             
-//            ChipView(title: "Nexters")
-//            ChipView(title: "Nexters", imageName: "")
+            ToolbarItem(placement: .topBarLeading) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "arrow.backward")
+                        .foregroundColor(.black)
+                }
+            }
         }
-        .padding(.horizontal, 20)
-        .padding(.vertical, 24)
-        .frame(width: 320, alignment: .center)
-        .background(.white)
-        .cornerRadius(20)
-        .overlay(
-            RoundedRectangle(cornerRadius: 20)
-                .inset(by: 0.5)
-                .stroke(Color(red: 0.89, green: 0.89, blue: 0.89), lineWidth: 1)
-        )
     }
 }
 
 #Preview {
-    ProfileView()
+    NavigationStack {
+        ProfileView()
+    }
+}
+
+struct ProfileChipRow: View {
+    
+    enum ViewType: String {
+        case 직군
+        case 동아리
+        case MBTI
+        case 별자리
+        case 지하철
+    }
+    
+    /// 행의 좌측에 붙는 타이틀
+    private(set) var type: ViewType
+    /// 프로필
+    private(set) var profile: Profile
+    
+    init(_ type: ViewType, _ profile: Profile) {
+        self.type = type
+        self.profile = profile
+    }
+    
+    var body: some View {
+        HStack(spacing: 0) {
+            Text(type.rawValue)
+                .multilineTextAlignment(.leading)
+                .lineLimit(0)
+                .font(.system(size: 14))
+                .foregroundColor(Color(red: 0.18, green: 0.18, blue: 0.18))
+                .frame(width: 52, alignment: .leading)
+            
+            // TODO: - 성민이 아이템으로 교체
+//            ChipView(title: "Nexters", imageName: "")
+            
+            flexibleChipView
+            
+            Spacer()
+        }
+    }
+    
+    private var flexibleChipView: some View {
+        switch type {
+        case .직군:
+            let major = profile.major
+                .compactMap { $0 }
+                .map { $0 }
+                .first!
+            return ChipView(title: major.name, imageName: major.imageName)
+        case .동아리:
+            let club = profile.club
+                .compactMap { $0 }
+                .map { $0 }
+                .first!
+            return ChipView(title: club.name, imageName: club.imageName)
+        case .MBTI:
+            let mbti = profile.mbti
+            return ChipView(title: mbti)
+        case .별자리:
+            let constellation = profile.constellation
+            return ChipView(title: constellation)
+        case .지하철:
+            let subwayName = profile.subwayName
+            return ChipView(title: subwayName)
+        }
+    }
 }
