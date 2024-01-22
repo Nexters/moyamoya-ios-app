d25f88e2d0731674fbb4e08fb70c1a480a4234f3
seongmin221 <seongmin221@naver.com>
2024-01-22 15:57:09 +0900

    overall ProfileEditorView wireframe implemented

--- FunchApp/Presentation/ProfileEditorScene/ProfileEditorView.swift
+++ FunchApp/Presentation/ProfileEditorScene/ProfileEditorView.swift
@@ -12,125 +12,90 @@ struct ProfileEditorView: View {
     @State var profile: Profile = .testableValue
     
     var body: some View {
-        VStack(alignment: .leading) {
-            Text("프로필 만들기")
-                .font(.system(size: 20))
-                .fontWeight(.bold)
-            
-            Text("프로필을 바탕으로 매칭을 도와드려요")
-                .font(.system(size: 14))
-            
-            VStack(alignment: .leading, spacing: 16) {
-                HStack(alignment: .center, spacing: 0) {
-                    Text("닉네임")
-                        .font(.system(size: 14))
-                        .frame(width: 52, alignment: .leading)
-                    
-                    FunchTextField(onSubmit: { /* action */ },
-                                   bindingText: $profile.userNickname,
-                                   placeholder: "test",
-                                   rightImage: Image(systemName: "magnifyingglass"))
-                }
-                
-                HStack(alignment: .center, spacing: 0) {
-                    Text("직군")
-                        .font(.system(size: 14))
-                        .frame(width: 52, alignment: .leading)
-                    
-                    jobSelectionView
-                }
-                
-                HStack(alignment: .top, spacing: 0) {
-                    Text("동아리")
-                        .font(.system(size: 14))
-                        .frame(width: 52, height: 48, alignment: .leading)
-                    
-                    clubSelectionView
-                }
-                
-                HStack(alignment: .top, spacing: 0) {
-                    Text("MBTI")
-                        .font(.system(size: 14))
-                        .frame(width: 52, height: 48, alignment: .leading)
-                    
-                    HStack(spacing: 8) {
-                        ProfileMBTIButtonPair(onTap: { selected in /* action */ },
-                                              dataPair: ["E", "I"])
-                        ProfileMBTIButtonPair(onTap: { selected in /* action */ },
-                                              dataPair: ["N", "S"])
-                        ProfileMBTIButtonPair(onTap: { selected in  /* action */ },
-                                              dataPair: ["F", "T"])
-                        ProfileMBTIButtonPair(onTap: { selected in  /* action */ },
-                                              dataPair: ["P", "J"])
-                    }
-                }
-                
-                HStack(alignment: .top, spacing: 0) {
-                    Text("생일")
-                        .font(.system(size: 14))
-                        .frame(width: 52, height: 56, alignment: .leading)
-                    
-                    VStack(alignment: .leading) {
-                        FunchTextField(onSubmit: { /* action */ },
-                                       bindingText: $profile.birth,
-                                       placeholder: "test")
-                        
-                        HStack(spacing: 0) {
-                            Spacer()
-                                .frame(width: 8)
-                            
-                            Image(systemName: "info.circle")
-                                .resizable()
-                                .frame(width: 16, height: 16)
-                                .foregroundColor(Color(red: 0.73, green: 0.73, blue: 0.73))
-
-                            Spacer()
-                                .frame(width: 4)
-                            
-                            Text("나이는 상대에게 공개되지 않아요")
-                                .font(.system(size: 12))
-                                .foregroundColor(Color(red: 0.73, green: 0.73, blue: 0.73))
-                            
-                            Spacer()
-                        }
-                    }
-                }
-                
-                HStack(alignment: .center, spacing: 0) {
-                    Text("지하철")
-                        .font(.system(size: 14))
-                        .frame(width: 52, alignment: .leading)
-                    
-                    FunchTextField(onSubmit: { /* action */ },
-                                   bindingText: $profile.subwayName,
-                                   placeholder: "지하철",
-                                   leftImage: Image(systemName: "magnifyingglass"))
-                }
-            }
-            .padding(.vertical, 24)
-            .padding(.horizontal, 20)
-            .background(Color.white)
-            .clipShape(RoundedRectangle(cornerRadius: 20))
-            .overlay {
-                RoundedRectangle(cornerRadius: 20)
-                    .stroke(Color(red: 0.84, green: 0.84, blue: 0.84), lineWidth: 1)
-            }
-            
-            Spacer()
-                .frame(height: 24)
-            
-            Button {
-                /* action */
-            } label: {
-                Text("이제 매칭할래요!")
-                    .font(.system(size: 18))
+        ScrollView {
+            VStack(alignment: .leading) {
+                Text("프로필 만들기")
+                    .font(.system(size: 20))
                     .fontWeight(.bold)
-                    .frame(height: 32)
-                    .frame(maxWidth: .infinity)
+                
+                Text("프로필을 바탕으로 매칭을 도와드려요")
+                    .font(.system(size: 14))
+                
+                VStack(alignment: .leading, spacing: 16) {
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("닉네임")
+                            .font(.system(size: 14))
+                            .frame(width: 52, alignment: .leading)
+                        
+                        FunchTextField(bindingText: $profile.userNickname,
+                                       placeholder: "최대 00글자")
+                    }
+                    
+                    HStack(alignment: .center, spacing: 0) {
+                        Text("직군")
+                            .font(.system(size: 14))
+                            .frame(width: 52, alignment: .leading)
+                        
+                        jobSelectionView
+                    }
+                    
+                    HStack(alignment: .top, spacing: 0) {
+                        Text("동아리")
+                            .font(.system(size: 14))
+                            .frame(width: 52, height: 48, alignment: .leading)
+                        
+                        clubSelectionView
+                    }
+                    
+                    HStack(alignment: .top, spacing: 0) {
+                        Text("MBTI")
+                            .font(.system(size: 14))
+                            .frame(width: 52, height: 48, alignment: .leading)
+                        
+                        mbtiSelectionView
+                    }
+                    
+                    HStack(alignment: .top, spacing: 0) {
+                        Text("생일")
+                            .font(.system(size: 14))
+                            .frame(width: 52, height: 56, alignment: .leading)
+                        
+                        birthInputField
+                    }
+                    
+                    HStack(alignment: .top, spacing: 0) {
+                        Text("지하철")
+                            .font(.system(size: 14))
+                            .frame(width: 52, height: 56, alignment: .leading)
+                        
+                        subwayInputField
+                    }
+                }
+                .padding(.vertical, 24)
+                .padding(.horizontal, 20)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 20))
+                .overlay {
+                    RoundedRectangle(cornerRadius: 20)
+                        .stroke(Color(red: 0.84, green: 0.84, blue: 0.84), lineWidth: 1)
+                }
+                
+                Spacer()
+                    .frame(height: 24)
+                
+                Button {
+                    /* action */
+                } label: {
+                    Text("이제 매칭할래요!")
+                        .font(.system(size: 18))
+                        .fontWeight(.bold)
+                        .frame(height: 32)
+                        .frame(maxWidth: .infinity)
+                }
+                .buttonStyle(.funch)
             }
-            .buttonStyle(.funch)
+            .padding(.horizontal, 20)
         }
-        .padding(.horizontal, 20)
     }
     
     
@@ -140,6 +105,8 @@ struct ProfileEditorView: View {
             ForEach(Profile.Major.dummies, id: \.self) { major in
                 Button {
                     self.selectedJobs = major
+                    // action
+                    print(selectedJobs)
                 } label: {
                     ChipView(title: major.name,
                              imageName: major.imageName,
@@ -161,6 +128,8 @@ struct ProfileEditorView: View {
                 Button {
                     if selectedClubs.contains(club) { selectedClubs.remove(club) }
                     else { selectedClubs.insert(club) }
+                    // action
+                    print(selectedClubs)
                 } label: {
                     ChipView(title: club.name,
                              imageName: club.imageName,
@@ -171,6 +140,90 @@ struct ProfileEditorView: View {
                 .buttonStyle(.noEffect)
                 .clipShape(RoundedRectangle(cornerRadius: 16))
             }
+        }
+    }
+    
+    
+    @State var selectedMBTI: [String] = .init(repeating: String(), count: 4)
+    let mbtiPairData: [[String]] = [["E", "I"], ["N", "S"], ["F", "T"], ["P", "J"]]
+    private var mbtiSelectionView: some View {
+        HStack(spacing: 8) {
+            ForEach(0..<4) { i in
+                ProfileMBTIButtonPair(onTap: { index in
+                    selectedMBTI[i] = mbtiPairData[i][index]
+                    // action
+                    print(selectedMBTI)
+                }, dataPair: mbtiPairData[i])
+            }
+        }
+    }
+    
+    
+    private var birthInputField: some View {
+        VStack(alignment: .leading) {
+            FunchTextField(bindingText: $profile.birth,
+                           placeholder: "생년월일 6자리 (ex: 991114)")
+            .keyboardType(.numberPad)
+            .onChange(of: profile.birth) { _, newValue in
+                // action
+            }
+            .onSubmit {
+                // action
+            }
+            
+            HStack(spacing: 0) {
+                Spacer()
+                    .frame(width: 8)
+                
+                Image(systemName: "info.circle")
+                    .resizable()
+                    .frame(width: 16, height: 16)
+                    .foregroundColor(Color(red: 0.73, green: 0.73, blue: 0.73))
+
+                Spacer()
+                    .frame(width: 4)
+                
+                Text("나이는 상대에게 공개되지 않아요")
+                    .font(.system(size: 12))
+                    .foregroundColor(Color(red: 0.73, green: 0.73, blue: 0.73))
+                
+                Spacer()
+            }
+        }
+    }
+    
+    struct SubwayName: Hashable { var name: String }
+    @State var subwayRecommendation: [SubwayName] = [.init(name: "test")]
+    private var subwayInputField: some View {
+        VStack(alignment: .leading) {
+            FunchTextField(bindingText: $profile.subwayName,
+                           placeholder: "지하철",
+                           leftImage: Image(systemName: "magnifyingglass"))
+            .onChange(of: profile.subwayName) { _, newValue in
+                // action
+                subwayRecommendation.append(.init(name: newValue))
+                print(subwayRecommendation)
+            }
+            
+            ScrollView(.horizontal) {
+                HStack {
+                    ForEach(subwayRecommendation, id: \.self) { subway in
+                        Button {
+                            print(subway.name)
+                            profile.subwayName = subway.name
+                        } label: {
+                            Text(subway.name)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .font(.system(size: 14))
+                                .foregroundStyle(.black)
+                                .padding(8)
+                        }
+                        .background(Color.gray)
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
+                    }
+                }
+            }
+            .scrollIndicators(.hidden)
         }
     }
 }
