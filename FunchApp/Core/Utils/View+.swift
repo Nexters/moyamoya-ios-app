4968ca1b489df48c6411821056bade8d3e19894a
seongmin221 <seongmin221@naver.com>
2024-01-22 22:08:01 +0900

    implement hide keyboard func

--- FunchApp/Core/Utils/View+.swift
+++ FunchApp/Core/Utils/View+.swift
@@ -5,4 +5,11 @@
 //  Created by 이성민 on 1/22/24.
 //
 
-import Foundation
+import SwiftUI
+
+extension View {
+    func hideKeyboard() {
+        /// 레퍼런스 `https://huniroom.tistory.com/entry/SwiftUI-실전형-TextField-사용하기-예제-MVVM-FocusState-ScrollView-movemunt`
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
