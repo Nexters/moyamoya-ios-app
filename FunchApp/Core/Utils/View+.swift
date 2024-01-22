a6b2b2c4321af122fbb9d26871978627a81b0ca5
seongmin221 <seongmin221@naver.com>
2024-01-22 23:46:56 +0900

    replaced comment on hideKeyboard()

--- FunchApp/Core/Utils/View+.swift
+++ FunchApp/Core/Utils/View+.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 extension View {
+    /// 레퍼런스 `https://huniroom.tistory.com/entry/SwiftUI-실전형-TextField-사용하기-예제-MVVM-FocusState-ScrollView-movemunt`
     func hideKeyboard() {
-        /// 레퍼런스 `https://huniroom.tistory.com/entry/SwiftUI-실전형-TextField-사용하기-예제-MVVM-FocusState-ScrollView-movemunt`
         UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
     }
 }
