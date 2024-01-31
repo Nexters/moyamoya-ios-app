52e6e47c9e12459c1e8049c4793cdb850b7ff7cb
seongmin221 <seongmin221@naver.com>
2024-01-31 20:43:08 +0900

    applied basic design systems

--- FunchApp/Presentation/ProfileEditorScene/ProfileEditorView.swift
+++ FunchApp/Presentation/ProfileEditorScene/ProfileEditorView.swift
@@ -9,91 +9,97 @@ import SwiftUI
 
 struct ProfileEditorView: View {
     
+    @State private var buttonIsEnabled: Bool = false
     @State var profile: Profile = .emptyValue
     
     var body: some View {
-        VStack(spacing: 0) {
+        ZStack {
+            Color.gray900
+                .ignoresSafeArea(.all)
             
-            ScrollView {
-                VStack(alignment: .leading, spacing: 0) {
-                    
-                    Spacer()
-                        .frame(height: 8)
-                    
-                    Text("프로필 만들기")
-                        .font(.system(size: 20))
-                        .fontWeight(.bold)
-                    
-                    Spacer()
-                        .frame(height: 2)
-                    
-                    Text("프로필을 바탕으로 매칭을 도와드려요")
-                        .font(.system(size: 14))
-                    
-                    Spacer()
-                        .frame(height: 24)
-                    
-                    VStack(alignment: .leading, spacing: 16) {
-                        ProfileInputRow(type: .닉네임, profile: $profile)
-                        ProfileInputRow(type: .직군, profile: $profile)
-                        ProfileInputRow(type: .동아리, profile: $profile)
-                        ProfileInputRow(type: .MBTI, profile: $profile)
-                        ProfileInputRow(type: .생일, profile: $profile)
-                        ProfileInputRow(type: .지하철, profile: $profile)
+            VStack(spacing: 0) {
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 0) {
+                        
+                        Spacer()
+                            .frame(height: 8)
+                        
+                        Text("프로필 만들기")
+                            .foregroundStyle(.white)
+                            .customFont(.title2)
+                        
+                        Spacer()
+                            .frame(height: 2)
+                        
+                        Text("프로필을 바탕으로 매칭을 도와드려요")
+                            .foregroundStyle(.gray300)
+                            .customFont(.body)
+                        
+                        Spacer()
+                            .frame(height: 24)
+                        
+                        VStack(alignment: .leading, spacing: 16) {
+                            ProfileInputRow(type: .닉네임, profile: $profile)
+                            ProfileInputRow(type: .직군, profile: $profile)
+                            ProfileInputRow(type: .동아리, profile: $profile)
+                            ProfileInputRow(type: .MBTI, profile: $profile)
+                            ProfileInputRow(type: .생일, profile: $profile)
+                            ProfileInputRow(type: .지하철, profile: $profile)
+                        }
+                        
+                        Spacer()
+                            .frame(height: 24)
                     }
-                    
-                    Spacer()
-                        .frame(height: 24)
+                    .padding(.horizontal, 20)
                 }
-                .padding(.horizontal, 20)
+                .onTapGesture {
+                    hideKeyboard()
+                }
+                
+                matchingButtonView
             }
-            .onTapGesture {
-                hideKeyboard()
+            .ignoresSafeArea(.container ,edges: .bottom)
+            .toolbar {
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button {
+                        /* action */
+                    } label: {
+                        Text("피드백 보내기")
+                            .foregroundStyle(.white)
+                            .customFont(.body)
+                            .padding(.horizontal, 12)
+                            .padding(.vertical, 8)
+                            .background(.gray800)
+                            .clipShape(RoundedRectangle(cornerRadius: 12.0))
+                    }
+                }
             }
             
-            matchingButtonView
         }
-        .toolbar {
-            ToolbarItem(placement: .topBarTrailing) {
-                Button {
-                    /* action */
-                } label: {
-                    Text("피드백 보내기")
-                        .font(.system(size: 14, weight: .regular))
-                        .foregroundColor(.white)
-                        .padding(.horizontal, 12)
-                        .padding(.vertical, 8)
-                        .background(.black)
-                        .clipShape(RoundedRectangle(cornerRadius: 12.0))
-                }
-            }
-        }
-        .ignoresSafeArea(edges: .bottom)
     }
-    
     
     
     private var matchingButtonView: some View {
         VStack(spacing: 0) {
-            Spacer()
-                .frame(height: 16)
             
             Button {
                 /* action */
+                buttonIsEnabled.toggle()
             } label: {
                 Text("이제 매칭할래요!")
-                    .font(.system(size: 18))
-                    .fontWeight(.bold)
-                    .frame(height: 32)
+                    .foregroundStyle(.gray900)
+                    .customFont(.subtitle1)
                     .frame(maxWidth: .infinity)
             }
-            .buttonStyle(.funch)
-            .padding(.horizontal, 20)
+            .buttonStyle(DefaultFunchButtonStyle(isEnabled: buttonIsEnabled))
             
             Spacer()
+                .frame(height: 34)
         }
-        .frame(height: 114)
-        .background(Color.gray)
+        .padding(.top, 16)
+        .padding(.horizontal, 20)
+        .frame(maxWidth: .infinity)
+        .background(.gray900)
     }
 }
 
