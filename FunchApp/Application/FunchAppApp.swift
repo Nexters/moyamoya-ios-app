a3f9fe20aff44f67aa0edcbe844db35805e7a663
GeonWoo <lgw9898@naver.com>
2024-02-04 15:01:06 +0900

    불필요한 코드 삭제

--- FunchApp/Application/FunchAppApp.swift
+++ FunchApp/Application/FunchAppApp.swift
@@ -9,35 +9,11 @@ import SwiftUI
 
 @main
 struct FunchAppApp: App {
-    let createUseCase = CreateProfileUseCase()
-    let fetchProfileUseCase = FetchProfileUseCase()
-    
     var body: some Scene {
         WindowGroup {
             NavigationStack {
                 ProfileView()
-                    .onAppear {
-//                        fetchProfileUseCase.fetchProfileFromDeviceId { result in
-//                            switch result {
-//                            case .success(let success):
-//                                break
-//                            case .failure(let failure):
-//                                break
-//                            }
-//                        }
-                        
-                        fetchProfileUseCase.fetchProfileFromId(query: .init(id: "65bdd58cebe5db753688b9fb")) { result in
-                            switch result {
-                            case .success(let success):
-                                break
-                            case .failure(let failure):
-                                break
-                            }
-                        }
-                    }
-            }
-            
-            
+            }   
         }
     }
 }
