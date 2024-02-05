79102fba8142ecdd48c981ae47a2bb95027762b2
seongmin221 <seongmin221@naver.com>
2024-02-05 15:15:28 +0900

    implemented ChipButton

---
+++ FunchApp/Core/DesignSystem/Sources/UICompoents.SwiftUI/ChipButton.swift
@@ -0,0 +1,18 @@
+//
+//  ChipButton.swift
+//  FunchApp
+//
+//  Created by 이성민 on 2/5/24.
+//
+
+import SwiftUI
+
+struct ChipButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChipButton()
+}
