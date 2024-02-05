3f105b5115039a86ba349840899857116e779d02
seongmin221 <seongmin221@naver.com>
2024-02-05 15:16:34 +0900

    implemented subway textfield

--- FunchApp/Presentation/ProfileEditorScene/ProfileInputRow.swift
+++ FunchApp/Presentation/ProfileEditorScene/ProfileInputRow.swift
@@ -63,19 +63,6 @@ struct ProfileInputRow: View {
             
         case .MBTI:
             mbtiInputField
-//            var selectedMBTI: [String] = .init(repeating: String(), count: 4)
-            /// Profile.MBTI에 적용될 값들입니다. 각각 쌍으로 매칭해두었습니다.
-            // FIXME: 나중에 다른 곳에 빼두면 좋을 것 같아요
-//            let mbtiPairData: [[String]] = [["E", "I"], ["N", "S"], ["F", "T"], ["P", "J"]]
-//            DynamicHGrid(itemSpacing: 8, lineSpacing: 8) {
-//                ForEach(0..<4) { pairIndex in
-//                    ProfileMBTIButtonPair(onTap: { index in
-//                        selectedMBTI[pairIndex] = mbtiPairData[pairIndex][index]
-//                        // action
-//                        print(selectedMBTI)
-//                    }, dataPair: mbtiPairData[pairIndex])
-//                }
-//            }
             
         case .혈액형:
             bloodTypeInputField
@@ -127,7 +114,6 @@ extension ProfileInputRow {
                         if selectedClub.contains(club) { selectedClub.remove(club) }
                         else { selectedClub.insert(club) }
                         profile.clubs = Array(selectedClub)
-                        print(profile.clubs)
                     },
                     title: club.name,
                     imageName: club.imageName,
@@ -149,116 +135,35 @@ extension ProfileInputRow {
     
     @ViewBuilder
     private var subwayInputField: some View {
+        @State var inputString: String = ""
         /// 지하철 검색하면 받아와지는 유사 지하철역 이름 리스트
         let subwayRecommendation: [SubwayInfo] = [.testableValue, .testableValue]
-        Text("test")
-    }
-}
-
-
-
-struct ChipButton: View {
-    
-    enum ViewType {
-        /// 텍스트
-        case text
-        /// 이미지 + 텍스트
-        case image
-    }
-    
-    /// 어떤 view인지에 따른 가로 padding
-    private var verticalPadding: CGFloat {
-        switch type {
-        case .text: 12
-        case .image: 8
-        }
-    }
-    
-    /// 어떤 view인지에 따른 세로 padding
-    private var horizontalPadding: CGFloat {
-        switch type {
-        case .text: 16
-        case .image: 8
-        }
-    }
-    
-    
-    /// 칩 뷰 타입
-    private(set) var type: ViewType
-    
-    init(action: @escaping () -> Void,
-//         isSelected: Binding<Bool>,
-         title: String,
-         imageName: String? = nil,
-         isSelected: Bool
-    ) {
-        self.action = action
-//        self._isSelected = isSelected
-        self.title = title
-        self.isSelected = isSelected
         
-        if let imageName = imageName {
-            self.type = .image
-            self.imageName = imageName
-        } else {
-            self.type = .text
-            self.imageName = ""
-        }
-    }
-    
-    /// 터치 액션
-    var action: () -> Void
-    /// 선택됐는지를 나타내는 State
-    var isSelected: Bool
-//    @Binding private var isSelected: Bool
-    /// 타이틀
-    var title: String = ""
-    /// 리소스 이름
-    var imageName: String = ""
-    
-    
-    var body: some View {
-        Button {
-            action()
-        } label: {
-            buttonInnerView
-        }
-        .tint(.white)
-        .padding(.vertical, verticalPadding)
-        .padding(.horizontal, horizontalPadding)
-        .padding(.trailing, type == .image ? 8 : 0)
-        .background(isSelected ? .gray500 : .gray800)
-        .clipShape(RoundedRectangle(cornerRadius: 16))
-    }
-    
-    @ViewBuilder
-    private var buttonInnerView: some View {
-        switch type {
-        case .text:
-            Text(title)
-                .foregroundStyle(isSelected ? .white : .gray400)
-                .customFont(.subtitle2)
-                .frame(width: 16, height: 24)
+        VStack(spacing: 0) {
+            FunchTextField(text: $inputString,
+                           placeholderText: "가까운 지하철역 검색",
+                           leadingImage: .init(systemName: "magnifyingglass"))
+            .onSubmit {
+                // api 통신
+            }
             
-        case .image:
-            HStack {
-                VStack {
-                    Image(systemName: imageName)
-                        .resizable()
-                        .frame(width: 18, height: 18)
+            Spacer()
+                .frame(height: 4)
+            
+            HStack(spacing: 4) {
+                ForEach(subwayRecommendation, id: \.self) { subwayInfo in
+                    Text(subwayInfo.name)
+                        .font(.Funch.body)
+                        .foregroundStyle(.white)
+                        .padding(8)
+                        .background(.gray500)
+                        .clipShape(RoundedRectangle(cornerRadius: 25.0))
                 }
-                .frame(width: 32, height: 32)
-                .background(.gray900)
-                .clipShape(RoundedRectangle(cornerRadius: 10))
-                
-                Text(title)
-                    .foregroundStyle(isSelected ? .white : .gray400)
-                    .customFont(.body)
+                Spacer()
             }
         }
     }
 }
-
 
 #Preview {
     NavigationStack {
