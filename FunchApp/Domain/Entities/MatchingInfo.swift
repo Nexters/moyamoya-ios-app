37ae8e1df92c8e6ba0fd9e8132844d2294b82724
seongmin221 <seongmin221@naver.com>
2024-02-12 17:32:00 +0900

    deleted uikit views

--- FunchApp/Domain/Entities/MatchingInfo.swift
+++ FunchApp/Domain/Entities/MatchingInfo.swift
@@ -33,3 +33,25 @@ struct MatchingInfo {
         var title: String
     }
 }
+
+extension MatchingInfo {
+    static var testableValue: MatchingInfo {
+        return MatchingInfo(
+            profile: .init(userCode: "ABCD",
+                           userNickname: "이성민",
+                           birth: "2000.02.21",
+                           majors: Profile.Major.dummies,
+                           clubs: Profile.Club.dummies,
+                           mbti: "ENTJ",
+                           bloodType: "A",
+                           subwayInfos: [.testableValue],
+                           viewerShip: "0"), 
+            similarity: 80,
+            synergyInfos: [.init(title: "찾았다, 내 소울메이트!", description: "ENTJ인 이성민님은 비전을 향해 적극적으로 이끄는 리더 타입!"),
+                           .init(title: "서로 다른 점을 찾는 재미", description: "A형인 이성민님은 호기심과 창의력을 갖췄지만 변덕스러워요"),
+                           .init(title: "7호선에서 만나요", description: "이성민님도 7호선에 살고 있어요")],
+            recommendInfos: [.init(title: "")],
+            subwayInfos: [.init(name: "상도역", lines: ["7"])]
+        )
+    }
+}
