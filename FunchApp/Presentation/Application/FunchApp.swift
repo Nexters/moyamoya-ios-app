5551fb10ea9b473a23ecd34f3af92b58e0b259bc
GeonWoo <lgw9898@naver.com>
2024-02-17 22:26:23 +0900

    .

--- FunchApp/Presentation/Application/FunchApp.swift
+++ FunchApp/Presentation/Application/FunchApp.swift
@@ -10,8 +10,8 @@ import SwiftUI
 @main
 struct FunchApp: App {
     @StateObject private var appCoordinator = AppCoordinator()
-    
     @StateObject var container: DIContainer = .init(services: Services())
+    
     @State private var isSplashing: Bool = true
     
     var body: some Scene {
