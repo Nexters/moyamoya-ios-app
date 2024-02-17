e53b437e17ca812f6eaee80f288fea5aa255b9c7
GeonWoo <lgw9898@naver.com>
2024-02-17 22:21:52 +0900

    added splash builder and connected view

--- FunchApp/Presentation/Application/FunchApp.swift
+++ FunchApp/Presentation/Application/FunchApp.swift
@@ -12,25 +12,39 @@ struct FunchApp: App {
     @StateObject private var appCoordinator = AppCoordinator()
     
     @StateObject var container: DIContainer = .init(services: Services())
+    @State var isLoading: Bool = true
     
     var body: some Scene {
         WindowGroup {
-            NavigationStack(path: $appCoordinator.paths) {
-                if !container.services.userService.profiles.isEmpty {
-                    HomeViewBuilder(container: container).body
-                } else {
-                    OnboardingViewBuilder(container: container).body
-                        .navigationDestination(for: AppCoordinatorPathType.self) { type in
-                            switch type {
-                            case let .onboarding(pathType):
-                                switch pathType {
-                                case .createProfile:
-                                    ProfileEditorView()
-                                        .navigationBarBackButtonHidden()
+            ZStack {
+                NavigationStack(path: $appCoordinator.paths) {
+                    if !container.services.userService.profiles.isEmpty {
+                        HomeViewBuilder(container: container).body
+                    } else {
+                        OnboardingViewBuilder(container: container).body
+                            .navigationDestination(for: AppCoordinatorPathType.self) { type in
+                                switch type {
+                                case let .onboarding(pathType):
+                                    switch pathType {
+                                    case .createProfile:
+                                        ProfileEditorView()
+                                            .navigationBarBackButtonHidden()
+                                    }
                                 }
                             }
-                        }
+                    }
                 }
+                .overlay {
+                    if isLoading {
+                        SplashViewBuilder(container: container).body
+                    }
+                }
+            }
+            
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.5, execute: {
+                    withAnimation { isLoading.toggle() }
+                })
             }
         }
         .environmentObject(appCoordinator)
