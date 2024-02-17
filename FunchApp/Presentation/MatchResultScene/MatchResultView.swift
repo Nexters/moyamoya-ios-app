c6c14d1016477409a4a048ba5e8f4bfd786dd14b
seongmin221 <seongmin221@naver.com>
2024-02-17 21:51:40 +0900

    implementing logic in progress

--- FunchApp/Presentation/MatchResultScene/MatchResultView.swift
+++ FunchApp/Presentation/MatchResultScene/MatchResultView.swift
@@ -13,15 +13,25 @@ final class MatchResultViewModel: ObservableObject {
     @Published var similarity: Int?
     @Published var chemistryInfos: [MatchingInfo.ChemistryInfo]?
     @Published var matchedProfile: MatchingInfo.MatchProfile?
-    //    @Published var isEqualMajor: Bool = false
-    //    @Published var isEqualClubs: Bool = false
-    //    @Published var isEqualMajor: Bool = false
-    //    @Published var isEqualMajor: Bool = false
-    //    @Published var isEqualMajor: Bool = false
+    
+    @Published var isEqualMajor: (String, Bool) = ("", false)
+    @Published var isEqualClubs: ([String], Bool) = ([""], false)
+    @Published var isEqualMBTI: (String, Bool) = ("", false)
+    @Published var isEqualBloodType: (String, Bool) = ("", false)
+    @Published var isEqualSubway: ([String], Bool) = ([""], false)
     
     enum Action {
         case fetchMatchResult
         case distributeInfo(MatchingInfo)
+        case isEqual(RowType)
+        
+        enum RowType {
+            case major
+            case clubs
+            case mbti
+            case bloodType
+            case subway
+        }
     }
     
     var container: DIContainer
@@ -40,6 +50,27 @@ final class MatchResultViewModel: ObservableObject {
             similarity = matchedInfo.similarity
             chemistryInfos = matchedInfo.chemistryInfos
             matchedProfile = matchedInfo.profile
+            
+        case let .isEqual(type):
+            let my = container.services.userService.profiles.last ?? .emptyValue
+            let other = container.services.userService.matchedResults.first?.recommendInfos
+            switch type {
+            case .major:
+                my.majors.first == other.
+                
+            case .clubs:
+                
+                
+            case .mbti:
+                
+                
+            case .bloodType:
+                
+                
+            case .subway:
+                
+                
+            }
         }
     }
 }
@@ -104,7 +135,7 @@ struct MatchResultView: View {
         switch pageIndex {
         case 0: chemistryView
         case 1: recommendationView
-        case 2: profileView(viewModel.matchedProfile ?? .empty)
+        case 2: matchedProfileView
         default: EmptyView()
         }
     }
@@ -191,29 +222,95 @@ struct MatchResultView: View {
     }
     
     /// 상대방의 프로필을 보여주는 View
-    @ViewBuilder
-    private func profileView(_ matchResult: MatchingInfo.MatchProfile) -> some View {
-        let profile: ProfileChipRow.ProfileRowInfo = .init(major: matchResult.major,
-                                                           clubs: matchResult.clubs,
-                                                           mbti: matchResult.mbti,
-                                                           bloodType: matchResult.bloodType,
-                                                           subwayInfos: matchResult.subwayNames)
-        Text(matchResult.name)
-            .font(.Funch.title2)
-            .foregroundStyle(.white)
-            .frame(maxWidth: .infinity, alignment: .leading)
-        
-        Spacer()
-            .frame(height: 20)
-        
-        VStack(alignment: .leading, spacing: 16) {
-            ProfileChipRow(.직군, profile, isHighlighted: true)
-            ProfileChipRow(.동아리, profile, isHighlighted: false)
-            ProfileChipRow(.MBTI, profile, isHighlighted: false)
-            ProfileChipRow(.혈액형, profile, isHighlighted: true)
-            ProfileChipRow(.지하철, profile, isHighlighted: false)
+    //    @ViewBuilder
+    //    private func profileView(_ matchResult: MatchingInfo.MatchProfile) -> some View {
+    //        let profile: ProfileChipRow.ProfileRowInfo = .init(major: matchResult.major,
+    //                                                           clubs: matchResult.clubs,
+    //                                                           mbti: matchResult.mbti,
+    //                                                           bloodType: matchResult.bloodType,
+    //                                                           subwayInfos: matchResult.subwayNames)
+    //        Text(matchResult.name)
+    //            .font(.Funch.title2)
+    //            .foregroundStyle(.white)
+    //            .frame(maxWidth: .infinity, alignment: .leading)
+    //
+    //        Spacer()
+    //            .frame(height: 20)
+    //
+    //        VStack(alignment: .leading, spacing: 16) {
+    //            ProfileChipRow(.직군, profile, isHighlighted: true)
+    //            ProfileChipRow(.동아리, profile, isHighlighted: false)
+    //            ProfileChipRow(.MBTI, profile, isHighlighted: false)
+    //            ProfileChipRow(.혈액형, profile, isHighlighted: true)
+    //            ProfileChipRow(.지하철, profile, isHighlighted: false)
+    //        }
+    //    }
+    
+    private var matchedProfileView: some View {
+        VStack(alignment: .leading, spacing: 0) {
+            Text(matchResult.name)
+                .font(.Funch.title2)
+                .foregroundStyle(.white)
+                .frame(maxWidth: .infinity, alignment: .leading)
+            
+            Spacer()
+                .frame(height: 20)
+            
+            VStack(alignment: .leading, spacing: 16) {
+                resultRow("직군", profileRow: majorRow)
+                resultRow("동아리", profileRow: clubsRow)
+                resultRow("MBTI", profileRow: mbtiRow)
+                resultRow("혈액형", profileRow: bloodRow)
+                resultRow("지하철", profileRow: subwayRow)
+            }
         }
     }
+    
+    
+    private func resultRow(_ label: String, profileRow: some View) -> some View {
+        HStack(alignment: .top, spacing: 0) {
+            Text(label)
+                .multilineTextAlignment(.leading)
+                .lineLimit(0)
+                .font(.Funch.body)
+                .foregroundStyle(.gray400)
+                .frame(width: 52, height: 48, alignment: .leading)
+            
+            profileRow
+            
+            Spacer()
+        }
+    }
+    
+    private var majorRow: some View {
+        ChipView(title: "", imageName: "")
+    }
+    
+    private var clubsRow: some View {
+        DynamicHGrid(itemSpacing: 8, lineSpacing: 8) {
+            ForEach(clubs, id: \.self) { club in
+                ChipView(title: club, imageName: club)
+            }
+        }
+    }
+    
+    private var mbtiRow: some View {
+        ChipView(title: mbti)
+    }
+    
+    private var bloodRow: some View {
+        ChipView(title: bloodType)
+    }
+    
+    private var subwayRow: some View {
+        HStack(spacing: 8) {
+            ForEach(subways, id: \.self) { subwayName in
+                ChipView(title: subwayName)
+            }
+        }
+    }
+    
+    
 }
 
 //#Preview {
