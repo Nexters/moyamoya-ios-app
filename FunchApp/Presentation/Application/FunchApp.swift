fe1658eb71027e9c3de6267fc47ec49aed0d9182
seongmin221 <seongmin221@naver.com>
2024-02-21 01:39:59 +0900

    cleaned code

--- FunchApp/Presentation/Application/FunchApp.swift
+++ FunchApp/Presentation/Application/FunchApp.swift
@@ -48,34 +48,5 @@ struct FunchApp: App {
             .environmentObject(appCoordinator)
             .environmentObject(container)
         }
-//        .environment(\.appCoordinator, AppCoordinator())
-//        .environment(\.diContainer, DIContainer(services: Services()))
-        
-        
-    }
-}
-
-
-struct AppCoordinatorKey: EnvironmentKey {
-    static var defaultValue: AppCoordinator = .init()
-}
-
-extension EnvironmentValues {
-    var appCoordinator: AppCoordinator {
-        get { self[AppCoordinatorKey.self] }
-        set { self[AppCoordinatorKey.self] = newValue }
-    }
-}
-
-
-
-struct DIContainerKey: EnvironmentKey {
-    static var defaultValue: DIContainer = .init(services: Services())
-}
-
-extension EnvironmentValues {
-    var diContainer: DIContainer {
-        get { self[DIContainerKey.self] }
-        set { self[DIContainerKey.self] = newValue }
     }
 }
