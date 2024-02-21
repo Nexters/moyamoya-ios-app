b9be066d008df4f013b57568c945bb4a63fdeb4f
seongmin221 <seongmin221@naver.com>
2024-02-21 16:33:07 +0900

    implemented delete profile usecase

--- FunchApp/Domain/UseCase/Profile/DeleteProfileUseCase.swift
+++ FunchApp/Domain/UseCase/Profile/DeleteProfileUseCase.swift
@@ -6,3 +6,28 @@
 //
 
 import Foundation
+
+protocol DeleteProfileUseCaseType {
+    func deleteProfile(completion: @escaping (Result<Void, Error>) -> Void)
+}
+
+final class DeleteProfileUseCase: DeleteProfileUseCaseType {
+    private let profileRepository: ProfileRepositoryType
+    
+    init() {
+        self.profileRepository = ProfileRepository()
+    }
+    
+    func deleteProfile(completion: @escaping (Result<Void, Error>) -> Void) {
+        profileRepository.deleteProfile { result in
+            switch result {
+            case .success(let result):
+                DispatchQueue.main.async {
+                    completion(.success(result))
+                }
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+    }
+}
